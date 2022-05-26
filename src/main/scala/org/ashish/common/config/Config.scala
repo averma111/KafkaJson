a27adbfe70019ea616f74a268e92bda3dce69947
averma111 <ashishn8751@outlook.com>
2022-05-26 16:06:06 -0700

    Refactored the code

--- src/main/scala/org/ashish/common/config/Config.scala
+++ src/main/scala/org/ashish/common/config/Config.scala
@@ -1,19 +1,15 @@
 package org.ashish.common.config
 
-import com.typesafe.config
 import com.typesafe.config.ConfigFactory
 
-class  Config {
+class Config {
 
   def getKafkaProperties = {
-    val configFile = new java.io.File("src/main/resources/KAFKA/kafka.properties")
-    val kafkaConfig = ConfigFactory.parseFile(configFile)
-    kafkaConfig
+    ConfigFactory.parseFile(new java.io.File("KAFKA/kafka.conf"))
   }
 
   def getPubSubProperties = {
-    val configFile = new java.io.File("src/main/resources/PUBSUB/pubsub.properties")
-    val pubsubConfig = ConfigFactory.parseFile(configFile)
-    pubsubConfig
+    ConfigFactory.parseFile(new java.io.File("PUBSUB/pubsub.conf"))
+
   }
 }
--- src/main/scala/org/ashish/driver/StartJob.scala
+++ src/main/scala/org/ashish/driver/StartJob.scala
@@ -1,6 +1,5 @@
 package org.ashish.driver
 
-import com.google.cloud.spark.bigquery.repackaged.com.google.cloud.bigquery.Option
 import org.ashish.common.config.Config
 import org.ashish.common.spark.SparkSingleton
 import org.ashish.impl.Logging
@@ -13,25 +12,27 @@ object StartJob extends Logging {
 
   def main(args: Array[String]): Unit = {
     if (args.length < 1) {
-      println("Enter the valid pipeline parameter<Kafka or PubSub")
+      println("Enter the valid pipeline parameter<Kafka or PubSub>")
       exit(1)
+
     }
 
   args(0) match {
-      case "Kafka" => {
+      case "Kafka" =>
         val config = new Config
+
         val scalaProducer = new ScalaProducer()
         scalaProducer.send(args, config.getKafkaProperties.getString("TOPIC"))
-      }
-      case "PubSub" => {
+
+      case "PubSub" =>
         val pubsubConfig = new Config
         val streamingContext = new SparkSingleton
         val ssc = streamingContext.createSparkStreamingContext
         val pubsub = new PubSub()
-        val topic = pubsubConfig.getPubSubProperties.getString("TOPIC")
         logger.warn("Creating the Dstream by calling consume method")
         val streamRecord = pubsub.consume(pubsubConfig.getPubSubProperties.getString("PROJECT_ID"),
-          pubsubConfig.getPubSubProperties.getString("SUBSCRIPTION"), ssc, Some(topic))
+          pubsubConfig.getPubSubProperties.getString("SUBSCRIPTION"), ssc,
+          Some(pubsubConfig.getPubSubProperties.getString("TOPIC")))
         logger.warn("Creating the spark dataset from streaming source")
         pubsub.createDataSetFromStream(streamRecord)
         logger.warn("Starting spark streaming context...")
@@ -40,7 +41,6 @@ object StartJob extends Logging {
         ssc.checkpoint(pubsubConfig.getPubSubProperties.getString("CHECKPOINT"))
         ssc.awaitTermination()
 
-      }
       case _ => println("Enter the valid pipeline name <Kafka Or PubSub>")
     }
   }
